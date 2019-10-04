838f382aa6fcc19c746a66afbc32c641aa117dda
Clifford Laserre <claserre9@gmail.com>
2019-10-04 11:10:50 -0400

    DAO complété
    Une page index.php a été ajouté pour tester la connexion avec le serveur

--- DAO/config.php
+++ DAO/config.php
@@ -1,3 +1,8 @@
-public interface DAO{
-	
+<?php
+interface IDAO{
+    const url = "localhost/420-AW6-RO";
+    const host = "localhost";
+    const dbname = "420-AW6-RO";
+    const user ="root";
+    const password="root";	
 }
