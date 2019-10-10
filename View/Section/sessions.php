77a433410a6f9b70cdeaebc5cee6f9c2e3d32469
XBricault <56260299+XBricault@users.noreply.github.com>
2019-10-10 18:08:27 -0400

    ajout de controlleur Session
    en attente d'une nouvelle table sessions pour sorter les groupes.

---
+++ View/Section/sessions.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="./style/css/style.css">
+<head>
+  <title><?=$titre?></title>
+  <?php include "CDN.php" ?>
+</head>
+<body>
+	<select>
+		<option value=""></option>
+	</select>
+</body>
+</html>
