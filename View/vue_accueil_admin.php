81a13cd4c18aa3fb8417cc82ead7a22974f12985
XBricault <56260299+XBricault@users.noreply.github.com>
2019-10-10 17:48:04 -0400

    modification
    de la page connection et ajout de la vu accueil professeur

--- View/vue_accueil_admin.php
+++ View/vue_accueil_admin.php
@@ -6,25 +6,26 @@
 <html lang="en">
 <link rel="stylesheet" type="text/css" href="../style/css/style.css">
 <head>
-  <title>Connexion</title>
+  <title>Accueil</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://kit.fontawesome.com/0e0877556c.js" crossorigin="anonymous"></script>
 </head>
 <body>
-	<img src="../style/images/rosemont.jpg" style="height:200px; display: block; margin:auto; margin-top: 5%;">
-	<form id="form-signin">
-		<h1>Connexion</h1>
+	<ul>
+	  <li><a>Tous les jours.</a></li>
+	  <li style="float:right"><a href="#about">Mon compte <i class="fas fa-user"></i></a></li>
+	</ul>
+	<div class="container-fluid" id="ctn_acc_admin">
+		<h1>Vos groupes :</h1>
 		<hr/>
-		<input type="text" id="utilisateur" class="form-control" placeholder="Numéro de DA" required autofocus>
-		<input type="password" id="password" class="form-control" placeholder="Mot de passe" required autofocus>
-		<hr/>
-		<button id="btn_connect" type="submit" >Se connecter</button>
-	</form>
-</div>
-
+		<select>
+			<option value=""></option>
+		</select>
+	</div>
 </body>
 </html>
