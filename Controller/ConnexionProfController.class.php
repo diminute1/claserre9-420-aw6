276799b6c0556710fd9b2ee909901dbbc14bdc35
Clifford Laserre <claserre9@gmail.com>
2019-10-10 17:57:36 -0400

    Minor Update

--- Controller/ConnexionProfController.class.php
+++ Controller/ConnexionProfController.class.php
@@ -1,11 +1,22 @@
 <?php
 require_once('./Controller/Action.interface.php');
 require_once('./View/page.class.php');
+require_once('./Model/ProfesseurDAO.class.php');
 
-class ConnexionProfController implements IAction
+class ConnexionprofController implements IAction
 {
 	public function execute()
 	{
-        
+		if (isset($_REQUEST['nom']) && isset($_REQUEST['prenom']) && isset($_REQUEST['courriel']) && isset($_REQUEST['motdepasse'])){
+			if (ProfesseurDAO::verifierCourriel($_REQUEST['courriel'])){
+				return new Page("connexionProf", "Connexion du professeur", null, null);
+				$_REQUEST["erreurConnexion"] = "Ce courriel existe déjà dans nos registres";
+			}else{
+				$professeur = ProfesseurDAO::ajouter($_REQUEST["nom"], $_REQUEST["prenom"], $_REQUEST["courriel"], $_REQUEST["motdepasse"]);
+				$_REQUEST["SuccesConnexion"] = "Ce courriel existe déjà dans nos registres";
+			}
+
+		}
+		return new Page("connexionProf", "Connexion du professeur", null, null);       
 	}
 }
---
+++ Model/ProfesseurDAO.class.php
@@ -0,0 +1,29 @@
+<?php
+require_once './Model/DAO/connexion.php';
+require_once './Model/Professeur.php';
+
+class ProfesseurDAO {
+
+public static function ajouter($nom, $prenom, $courriel, $motdepasse)
+    {
+        $db = Connection::getInstance();
+        $hash = password_hash($motdepasse, PASSWORD_DEFAULT);
+        $pstmt = $db->prepare("INSERT INTO professeur VALUES (?, ?, ?, ?)");
+        $pstmt->execute(array($courriel, $nom, $prenom, $hash));
+    }
+
+public static function verifierCourriel($courriel)
+    {
+        $db = Connection::getInstance();
+
+        $pstmt = $db->prepare("SELECT courriel FROM professeur WHERE courriel = :x");
+        $pstmt->execute(array(':x' => $courriel));
+        $result = $pstmt->fetch(PDO::FETCH_OBJ);
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
