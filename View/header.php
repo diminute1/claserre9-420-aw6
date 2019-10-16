d8dcb565e307c2bb0898ced41b5e8149cce0579d
XBricault <56260299+XBricault@users.noreply.github.com>
2019-10-16 15:32:05 -0400

    Terminé les vue
    Reste qu'a mettre les interactions DAO

--- View/header.php
+++ View/header.php
@@ -4,24 +4,42 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" type="text/css" href="../style/css/style.css">
+<link rel="stylesheet" type="text/css" href="../style/css/style_header.css">
 <head>
   <title>Accueil</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <script src="https://kit.fontawesome.com/0e0877556c.js" crossorigin="anonymous"></script>
 </head>
 <body>
-	<div class="header">
-		<ul>
-		  <li><a>Département d'éducation physique</a></li>
-		  <li style="float:right"><a href="#about"><?php echo(ISSET ($_SESSION["connected"]))? $_SESSION["connected"]: ("Mon compte") ?> <i class="fas fa-user"></i></a></li>
-		</ul>
+<button id="home_button"><i class="fas fa-user"></i></button>
+
+<div class="dropdown">
+	<button onclick="showmenu()" class="dropbtn"><?php echo(ISSET ($_SESSION["connected"]))? $_SESSION["connected"]: ("Mon compte ") ?><i class="fas fa-user"></i></button>
+	<div id="myDropdown" class="dropdown-content">
+		<a href="#">Link 1</a>
+		<a href="#">Déconnexion</a>
 	</div>
+</div> 
 </body>
+<script>
+function showmenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
+</script>
 </html>
 
--- View/vue_accueil_admin.php
+++ View/vue_accueil_admin.php
@@ -6,36 +6,35 @@
 <html lang="en">
 <link rel="stylesheet" type="text/css" href="../style/css/style.css">
 <head>
-  <title>Accueil</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <script src="https://kit.fontawesome.com/0e0877556c.js" crossorigin="anonymous"></script>
+	<title>Accueil</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 </head>
 <body>
 
-	<?php
-	include'header.php';	
-	?>
+<?php
+include"header.php";
+?>
+<img src="../style/images/rosemont.jpg" style="height:56.3px; display: block; margin-left: auto; margin-right: auto; top: 0%;">
+<div id="ctn_acc_admin">
+	<h2>Sélectionnez une session :</h2>
+	<hr/>
+	<select>
+		<option value="">Automne 2019</option>
+		<option value="">Hiver 2019</option>
+		<option value="">Automne 2018</option>
+	</select>
+	<hr/>
+	<h2>Sélectionnez un groupe :</h2>
+	<select>
+		<option value="">Groupe 1</option>
+		<option value="">Groupe Mardi</option>
+		<option value="">Groupe Cincinnati</option>
+	</select>
+</div>
 
-	<div class="container-fluid" id="ctn_acc_admin">
-		<h2>Sélectionnez une session :</h2>
-		<hr/>
-		<select>
-			<option value="">Automne 2019</option>
-			<option value="">Hiver 2019</option>
-			<option value="">Automne 2018</option>
-		</select>
-		<hr/>
-		<h2>Sélectionnez un groupe :</h2>
-		<select>
-			<option value="">Groupe 1</option>
-			<option value="">Groupe Mardi</option>
-			<option value="">Groupe Cincinnati</option>
-		</select>
-	</div>
 </body>
 </html>
