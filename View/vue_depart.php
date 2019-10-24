547f6ac91bf184e872ccaa93f356b506e1fb3aa1
XBricault <56260299+XBricault@users.noreply.github.com>
2019-10-24 17:02:41 -0400

    vue départ

--- View/vue_depart.php
+++ View/vue_depart.php
@@ -13,7 +13,7 @@ function getRandomImage($dir_path = NULL){
         if($count > 2){
             $index = rand(2, ($count-1));
             $filename = $files[$index];
-            return '<img src="'.$dir_path."/".$filename.'" alt="'.$filename.'">';
+            return $dir_path."/".$filename;
         } else {
             return "The directory is empty!";
         }
@@ -32,13 +32,40 @@ function getRandomImage($dir_path = NULL){
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 </head>
 <body>
-	<div class="cards_depart">
-		<?php echo getRandomImage("../style/images/images_rdm");?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12"><h1 style="text-align:center; margin-top:2%;">Bienvenue</h1><div/>
+	<div/>
+	<div class="row">
+		<div class="col-md-6" style="margin-top: 10%;">
+			<div class="card-deck flex-row flex-nowrap" style="width:100%; height: 100%;">
+				<a href="vue_connexion_admin.php">
+				<div class="card">
+					<img src="<?php echo getRandomImage("../style/images/images_rdm");?>" style="max-width:100%; width=100%;"></img>
+					<div class="card-body">
+						<h3 style="color: black;" class="card-sub align-middle">Connexion Administrateur</h3>
+						<p style="color: black;" class="time-card">Connectez vous et gérez vos groupes et étudiants!</p>
+					</div>
+				</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-md-6" style="margin-top: 10%;">
+			<div class="card-deck flex-row flex-nowrap" style="width:100%; height: 100%;">
+				<a href="vue_connexion_etudiant.php">
+				<div class="card">
+					<img src="<?php echo getRandomImage("../style/images/images_rdm");?>" style="max-width:100%; width=100%;"></img>
+					<div class="card-body">
+						<h3 style="color: black;" class="card-sub align-middle">Connexion Étudiant</h3>
+						<p style="color: black;"class="time-card">Connectez vous et enregistrer un entrainement hebdomadaire!</p>
+					</div>
+				</div>
+				</a>
+			</div>
+		</div>
 	</div>
-	<div class="cards_depart">
-		<?php echo getRandomImage("../style/images/images_rdm");?>
-	</div>
-
+</div>
+</div>
 </div>
 </body>
 </html>
