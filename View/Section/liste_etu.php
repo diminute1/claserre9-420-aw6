6b953c988006a88d86534d430eab9c10835506ea
XBricault <56260299+XBricault@users.noreply.github.com>
2019-10-28 10:49:41 -0400

    Update liste_etu.php

--- View/Section/liste_etu.php
+++ View/Section/liste_etu.php
@@ -1,20 +1,15 @@
 <div class="container list-group">
-	<a class="list-group-item list-group-item-action">
-
 		<?php
 		foreach ($data as $etu) { ?>
 		
 		<div class="d-flex">
-			<div class="p-2"><?=$etu->getId()." ".$etu->getNom()." ".$etu->getPrenom();?></div>
-				<div class="ml-auto p-2">
-				<div class="btn-group btn-group-sm" role="group">
+			<div class="list-group-item w-100"><?=$etu->getId()." ".$etu->getNom()." ".$etu->getPrenom();?>
+				<div class="btn-group btn-group-sm float-right" role="group">
 				<button type="button" class="btn btn-secondary btn-sm">Profil</button>
 				<button type="button" class="btn btn-secondary btn-sm">Activités</button>
 				</div>
 			</div>
 		</div>
-		<hr style="margin:0;">
 
 		<?php } ?>	
-   </a>
 </div> 
