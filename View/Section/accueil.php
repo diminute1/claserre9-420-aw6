78b915a4039bf5d19ea07c1509ab83b072b27fe9
XBricault <56260299+XBricault@users.noreply.github.com>
2019-11-14 10:33:57 -0500

    test dans Acceuil
    juste du css

--- View/Section/accueil.php
+++ View/Section/accueil.php
@@ -9,21 +9,41 @@
 	</div>
 </form>-->
 
+<?php
+function getRandomImage($dir_path = NULL){
+    if(!empty($dir_path)){
+        $files = scandir($dir_path);
+        $count = count($files);
+        if($count > 2){
+            $index = rand(2, ($count-1));
+            $filename = $files[$index];
+            return '<img style="width:538px;height:400px;" src="'.$dir_path."/".$filename.'" alt="'.$filename.'">';
+        } else {
+            return "The directory is empty!";
+        }
+    } else {
+        return "Please enter valid path to image directory!";
+    }
+}
+?>
+
+
+
+
 <div class="container">
 	<div class="card-deck">
 		<div class="card">
 			<a id="link" href="?action=portailprof">
-				<img class="card-img-top" src="https://dummyimage.com/600x400/000000/fff" alt="Professeur">
+				<?php echo getRandomImage("./Style/images/images_rdm");?>
 				<div class="card-body">
 					<h5 class="card-title">Professeur</h5>
 					<p class="card-text">Si vous êtes professeur du cours d'education physique et autonomie</p>
 				</div>
 			</a>
-
 		</div>
 		<div class="card">
 			<a id="link" href="?action=portailetu">
-				<img class="card-img-top" src="https://dummyimage.com/600x400/000000/fff" alt="Professeur">
+				<?php echo getRandomImage("./Style/images/images_rdm");?>
 				<div class="card-body">
 					<h5 class="card-title">Étudiants</h5>
 					<p class="card-text">Si vous êtes inscrits au cours d'éducation physique et autonomie</p>
