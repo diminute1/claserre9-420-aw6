3b1460d6daeb7fdeb1fd9629a4230847ec23e642
Laserre Clifford <claserre9@gmail.com>
2019-11-28 11:44:38 -0500

    vue profil etudiant coté prof terminé

--- View/Section/profiletu.php
+++ View/Section/profiletu.php
@@ -1,18 +1,14 @@
+    <?php
+    $invalide = false;
+    if (isset($_SESSION['type_utilisateur']) && $_SESSION['type_utilisateur'] == "professeur") {
+        $invalide = true;
+    } ?>
+
     <div class="container profile profile-view" id="profile">
-        <div class="row">
-            <div class="col-md-12 alert-col relative">
-                <div class="alert alert-info absolue center" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><span>Profile save with success</span></div>
-            </div>
-        </div>
         <form>
             <div class="form-row profile-row">
-                <div class="col-md-4 relative">
-                    <div class="avatar">
-                        <div class="avatar-bg center"></div>
-                    </div><input type="file" class="form-control" name="avatar-file">
-                </div>
-                <div class="col-md-8">
-                    <h1>Profil</h1>
+                <div class="col-md-12 offset-md-0">
+                    <h1 class="my-4">Profil</h1>
                     <hr>
                     <div class="form-row">
                         <div class="col-sm-12 col-md-6">
@@ -22,25 +18,27 @@
                             <div class="form-group"><label>Nom</label><input class="form-control" type="text" name="nom_etu" disabled=""></div>
                         </div>
                     </div>
-                    <div class="form-group"><label>Numéro d'admission</label><input class="form-control" type="email" autocomplete="off" required="" name="email" disabled=""></div>
+                    <div class="form-group"><label>Numéro d'admission</label><input class="form-control" type="text" autocomplete="off" required="" name="email" disabled=""></div>
                     <div class="form-row">
                         <div class="col-sm-12 col-md-6">
-                            <div class="form-group"><label>Taille</label><input class="form-control" type="password" name="taille_etu" autocomplete="off" required=""></div>
+                            <div class="form-group"><label>Taille</label><input class="form-control" type="text" name="taille_etu" autocomplete="off" <?php if ($invalide) echo 'disabled=""' ?>></div>
                         </div>
                         <div class="col-sm-12 col-md-6">
-                            <div class="form-group"><label>Âge</label><input class="form-control" type="password" name="age_etu" autocomplete="off" required=""></div>
+                            <div class="form-group"><label>Âge</label><input class="form-control" type="text" name="age_etu" autocomplete="off" <?php if ($invalide) echo 'disabled=""' ?>></div>
                         </div>
                         <div class="col-sm-12 col-md-6">
-                            <div class="form-group"><label>Poids</label><input class="form-control" type="password" name="poids_etu" autocomplete="off" required=""></div>
+                            <div class="form-group"><label>Poids</label><input class="form-control" type="text" name="poids_etu" autocomplete="off" <?php if ($invalide) echo 'disabled=""' ?>></div>
                         </div>
                         <div class="col-sm-12 col-md-6">
-                            <div class="form-group"><label>IMC</label><input class="form-control" type="password" name="imc_etu" autocomplete="off" required=""></div>
+                            <div class="form-group"><label>IMC</label><input class="form-control" type="text" name="imc_etu" autocomplete="off" <?php if ($invalide) echo 'disabled=""' ?>></div>
                         </div>
                     </div>
                     <hr>
-                    <div class="form-row">
-                        <div class="col-md-12 content-right"><button class="btn btn-primary form-btn" type="submit">Sauvegarder</button><button class="btn btn-danger form-btn" type="reset">Annuler</button></div>
-                    </div>
+                    <?php if (isset($_SESSION['type_utilisateur']) && $_SESSION['type_utilisateur'] == "etudiant") { ?>
+                        <div class="form-row">
+                            <div class="col-md-12 content-right"><button class="btn btn-primary form-btn mr-3" type="submit">Sauvegarder</button><button class="btn btn-danger form-btn" type="reset">Annuler</button></div>
+                        </div>
+                    <?php } ?>
                 </div>
             </div>
         </form>
